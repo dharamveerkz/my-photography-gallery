aad4cacfd9ee2fd996a8e44ce8baddc3b05b68ae
Dharamveer <102931119+dharamveerkz@users.noreply.github.com>
2026-04-26 11:29:24 +0530

    Update useIntersection.js

--- hooks/useIntersection.js
+++ hooks/useIntersection.js
@@ -1,1 +1,28 @@
 
+// src/hooks/useIntersection.js
+import { useEffect, useRef } from "react";
+
+export const useIntersection = (callback, options = {}) => {
+  const observerRef = useRef(null);
+  const targetRef = useRef(null);
+
+  useEffect(() => {
+    observerRef.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        callback();
+      }
+    }, {
+      rootMargin: "100px",
+      threshold: 0.01,
+      ...options
+    });
+
+    if (targetRef.current) {
+      observerRef.current.observe(targetRef.current);
+    }
+
+    return () => observerRef.current?.disconnect();
+  }, [callback]);
+
+  return targetRef;
+};
