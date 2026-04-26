5ff132ae3a01cc74f6cc2bc822717d1a7aa78724
Dharamveer <102931119+dharamveerkz@users.noreply.github.com>
2026-04-26 12:10:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Preload critical fonts for faster FCP
+const preloadFonts = () => {
+  const fontLink = document.createElement("link");
+  fontLink.rel = "preload";
+  fontLink.as = "font";
+  fontLink.type = "font/woff2";
+  fontLink.href = "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap";
+  fontLink.crossOrigin = "anonymous";
+  document.head.appendChild(fontLink);
+};
+preloadFonts();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
