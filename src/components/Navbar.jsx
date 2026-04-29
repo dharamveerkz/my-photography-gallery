2cd8b036c6db1f3de294763d3b335ffb072ca968
Dharamveer <102931119+dharamveerkz@users.noreply.github.com>
2026-04-29 15:08:18 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,6 +18,7 @@ const Navbar = () => {
     { name: "Home", path: "/" },
     { name: "About", path: "/about" },
     { name: "Contact", path: "/contact" },
+    { name: "Learn Now", path: "/basics" },
   ];
 
   return (
